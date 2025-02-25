363a4edbba65790379fbd8362f2d8609d0f5176a
TechAddict <rahil.311298@gmail.com>
2025-02-26 00:13:49 +0530

    Removal -> Deprecation labelling

--- src/app/components/landingpage/frameworks/FrameworksTitle.tsx
+++ src/app/components/landingpage/frameworks/FrameworksTitle.tsx
@@ -18,7 +18,7 @@ const FrameworksTitle = () => {
                         xs: '30px'
                     }
                 }}>Increase speed of your development and
-                    launch quickly with Modernize</Typography>
+                    launch quickly</Typography>
             </Grid>
         </Grid>
     );
